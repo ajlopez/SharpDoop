e3060da2c60bc493c30776ef9e7915385789d31e
ajlopez <webmaster@ajlopez.com>
2013-02-03 19:03:07 -0300

    Refactor Process to Map and Reduce

--- Src/SharpDoop.Core.Tests/JobTests.cs
+++ Src/SharpDoop.Core.Tests/JobTests.cs
@@ -10,12 +10,35 @@
     public class JobTests
     {
         [TestMethod]
-        public void RunJob()
+        public void RunJobMapReduce()
         {
             Job<int, string, string, int, string, int> job = new Job<int, string, string, int, string, int>(Map, Reduce);
             var items = new string[] { "a", "word", "is", "a", "word" };
             var pairs = items.Select((item, k) => new Pair<int, string>() { Key = k, Value = item });
-            var result = job.Process(pairs);
+            job.Map(pairs);
+            var result = job.Reduce();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(3, result.Keys.Count);
+            Assert.IsTrue(result.ContainsKey("a"));
+            Assert.IsTrue(result.ContainsKey("word"));
+            Assert.IsTrue(result.ContainsKey("is"));
+            Assert.AreEqual(2, result["a"]);
+            Assert.AreEqual(2, result["word"]);
+            Assert.AreEqual(1, result["is"]);
+        }
+
+        [TestMethod]
+        public void RunJobTwoMapsAndReduce()
+        {
+            Job<int, string, string, int, string, int> job = new Job<int, string, string, int, string, int>(Map, Reduce);
+            var items1 = new string[] { "a", "word", "is" };
+            var pairs1 = items1.Select((item, k) => new Pair<int, string>() { Key = k, Value = item });
+            var items2 = new string[] { "a", "word" };
+            var pairs2 = items2.Select((item, k) => new Pair<int, string>() { Key = k, Value = item });
+            job.Map(pairs1);
+            job.Map(pairs2);
+            var result = job.Reduce();
 
             Assert.IsNotNull(result);
             Assert.AreEqual(3, result.Keys.Count);
--- Src/SharpDoop.Core/Job.cs
+++ Src/SharpDoop.Core/Job.cs
@@ -9,6 +9,8 @@
     {
         private Action<K1, V1, MapContext<K2, V2>> map;
         private Action<K2, IList<V2>, ReduceContext<K3, V3>> reduce;
+        private MapContext<K2, V2> mapcontext = new MapContext<K2, V2>();
+        private ReduceContext<K3, V3> redcontext = new ReduceContext<K3, V3>();
 
         public Job(Action<K1, V1, MapContext<K2, V2>> map, Action<K2, IList<V2>, ReduceContext<K3, V3>> reduce)
         {
@@ -16,18 +18,18 @@
             this.reduce = reduce;
         }
 
-        public IDictionary<K3, V3> Process(IEnumerable<Pair<K1, V1>> keyvalues)
+        public void Map(IEnumerable<Pair<K1, V1>> keyvalues)
         {
-            var mapcontext = new MapContext<K2, V2>();
-            var redcontext = new ReduceContext<K3, V3>();
-
             foreach (var keyvalue in keyvalues)
-                map(keyvalue.Key, keyvalue.Value, mapcontext);
+                map(keyvalue.Key, keyvalue.Value, this.mapcontext);
+        }
 
+        public IDictionary<K3, V3> Reduce()
+        {
             foreach (var key in mapcontext.KeyValues.Keys)
-                reduce(key, mapcontext.KeyValues[key], redcontext);
+                reduce(key, this.mapcontext.KeyValues[key], this.redcontext);
 
-            return redcontext.KeyValues;
+            return this.redcontext.KeyValues;
         }
     }
 }
